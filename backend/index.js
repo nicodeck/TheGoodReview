37ded4e2efc64f90a4aef8186c8e41f2a2fa38bc
Nicolas Descamps <nicolas.descamps@student-cs.fr>
2023-11-24 19:05:48 +0100

    feat(backend): added route "images"

--- backend/index.js
+++ backend/index.js
@@ -5,8 +5,11 @@ const port = 3000;
 require("dotenv").config();
 
 const games = require("./src/routes/games.routes");
+const images = require("./src/routes/images.routes");
 
 app.use("/games", games);
+
+app.use("/images", images);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
---
+++ backend/src/routes/images.routes.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const router = express.Router();
+const axios = require("axios");
+
+const { igdb_api_request } = require("../utils/igdb_request.utils");
+
+router.get("/", (req, res) => {
+  res.send("image route");
+});
+
+router.get("/cover", async (req, res) => {
+  if (!req.query.id) {
+    res.status(400).send("Error: missing required query parameter: id.");
+  }
+  const coverSize = req.query.size ? req.query.size : "cover_big";
+
+  const imageInfo = await igdb_api_request(
+    "/covers",
+    process.env.CLIENT_ID,
+    process.env.CLIENT_SECRET,
+    `fields image_id; where id=${req.query.id};`
+  );
+
+  const imageId = imageInfo[0].image_id;
+
+  res.send(
+    `https://images.igdb.com/igdb/image/upload/t_${coverSize}/${imageId}.jpg`
+  );
+});
+
+module.exports = router;

f6369dd1e7c41c5fd5cfa3d7313f8febef78a98d
Nicolas Descamps <nicolas.descamps@student-cs.fr>
2023-11-28 00:04:19 +0100

    feat(frontend): GameCarousel static structure

--- frontend/src/components/GameCarousel/GameCarousel.tsx
+++ frontend/src/components/GameCarousel/GameCarousel.tsx
@@ -3,22 +3,38 @@ import "./GameCarousel.css";
 import GameCard from "./GameCard/GameCard";
 
 import { GameCardProps } from "./GameCard/GameCard";
+import { IoChevronBack, IoChevronForward } from "react-icons/io5";
 
 export interface GameCarouselProps {
-  carouselName: string;
+  carouselTitle: string;
   carouselGames: Array<GameCardProps>;
 }
 
-function GameCarousel({ carouselName, carouselGames }: GameCarouselProps) {
+function GameCarousel({ carouselTitle, carouselGames }: GameCarouselProps) {
   return (
-    <div className="game-carousel-container">
-      <h1>{carouselName}</h1>
-      {carouselGames.map(({ gameName, gameImageLink }: GameCardProps) => {
-        return <GameCard gameName={gameName} gameImageLink={gameImageLink} />;
-      })}
+    <div className="game-carousel">
+      <h1 className="game-carousel-title">{carouselTitle}</h1>
+      <div className="game-carousel-carousel-container">
+        <div className="game-carousel-left-arrow-container">
+          <div className="game-carousel-left-arrow">
+            <IoChevronBack size={32} />
+          </div>
+        </div>
+        <div className="game-carousel-carousel-games">
+          {carouselGames.map(({ gameName, gameImageLink }: GameCardProps) => {
+            return (
+              <GameCard gameName={gameName} gameImageLink={gameImageLink} />
+            );
+          })}
+        </div>
+        <div className="game-carousel-right-arrow-container">
+          <div className="game-carousel-right-arrow">
+            <IoChevronForward size={32} />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
-//TODO: il faut maper les jeux pour chaque carte et ajouter les parametres dans la homepage
 
 export default GameCarousel;

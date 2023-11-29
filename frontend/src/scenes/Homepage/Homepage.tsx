af303dbe6afe7eae76e61ae6b5bc929dcbfc700b
Nicolas Descamps <nicolas.descamps@student-cs.fr>
2023-11-29 22:47:12 +0100

    feat(frontend): searchbar is functionnal

--- frontend/src/scenes/Homepage/Homepage.tsx
+++ frontend/src/scenes/Homepage/Homepage.tsx
@@ -3,35 +3,61 @@ import { v4 as uuidv4 } from "uuid";
 import "./Homepage.css";
 
 import GameCard, { GameCardProps } from "@components/GameCard/GameCard";
-import { useLoaderData } from "react-router-dom";
 
 import homepageVideoGamesBackground from "./assets/img/video_games_background.jpg";
+import { useEffect, useState } from "react";
+import { useDebounce } from "usehooks-ts";
 
-interface HomepageData {
-  games: Array<GameCardProps>;
-}
-
-export async function loader() {
-  const homepageGamesData = await axios({
-    method: "GET",
-    url:
-      import.meta.env.VITE_BACKEND_URL +
-      ":" +
-      import.meta.env.VITE_BACKEND_PORT +
-      "/homepage",
-  })
-    .then((res) => {
-      return res.data;
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-  console.log("response", homepageGamesData);
-  return { games: homepageGamesData };
-}
+/*
+TODO: handle if search gives no result
+TODO: minimal page height
+*/
 
 function Homepage() {
-  const { games } = useLoaderData() as HomepageData;
+  const [searchText, setSearchText] = useState("");
+  const debouncedValue = useDebounce(searchText, 500);
+
+  const [games, setGames] = useState([]);
+
+  const searchInputHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newSearchText = e.target.value;
+    setSearchText(newSearchText);
+  };
+
+  useEffect(() => {
+    if (debouncedValue == "") {
+      axios({
+        method: "GET",
+        url:
+          import.meta.env.VITE_BACKEND_URL +
+          ":" +
+          import.meta.env.VITE_BACKEND_PORT +
+          "/homepage",
+      })
+        .then((res) => {
+          setGames(res.data);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    } else {
+      axios({
+        method: "GET",
+        url:
+          import.meta.env.VITE_BACKEND_URL +
+          ":" +
+          import.meta.env.VITE_BACKEND_PORT +
+          "/search?search_text=" +
+          debouncedValue,
+      })
+        .then((res) => {
+          setGames(res.data);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  }, [debouncedValue]);
   console.log(games);
 
   return (
@@ -44,7 +70,7 @@ function Homepage() {
       ></div>
       <div className="homepage-games-outer-container">
         <div className="homepage-searchbar">
-          <input type="text" />
+          <input type="text" value={searchText} onChange={searchInputHandler} />
         </div>
         <div className="homepage-games-inner-container">
           {games.map(({ gameName, gameImageLink }: GameCardProps) => {

573b5d9c6868ae4c16b84e606ca203deada1573b
Nicolas Descamps <nicolas.descamps@student-cs.fr>
2023-12-01 10:27:15 +0100

    feat(frontend): moved navbar background change feature to Navbar component

--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -1,24 +1,50 @@
 import "./Navbar.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import { IoMenuSharp, IoChevronUpSharp } from "react-icons/io5";
 
 import NavbarItem from "./components/NavbarItem/NavbarItem";
 import Logo from "./components/Logo/Logo";
 
-function Navbar() {
+interface NavbarProps {
+  backgroundChangesOnScroll: boolean;
+}
+
+function Navbar({ backgroundChangesOnScroll }: NavbarProps) {
   const [navbarMenuIsOpen, setNavbarMenuIsOpen] = useState(false);
 
+  const [scrollTop, setScrollTop] = useState(window.scrollY);
+
+  // useEffect to update scrollTop
+  useEffect(() => {
+    if (backgroundChangesOnScroll) {
+      const handleScroll = () => {
+        setScrollTop(window.scrollY);
+      };
+
+      window.addEventListener("scroll", handleScroll);
+
+      return () => {
+        window.removeEventListener("scroll", handleScroll);
+      };
+    }
+  }, [backgroundChangesOnScroll]);
+
   return (
-    <div className="navbar-container" id="navbar-container">
+    <div
+      className={
+        "navbar-container" +
+        (!navbarMenuIsOpen ? " navbar-items-container-is-hidden" : "") +
+        (backgroundChangesOnScroll && scrollTop < 50
+          ? " navbar-transparent"
+          : "")
+      }
+      id="navbar-container"
+    >
       <div className="navbar-logo-container">
         <Logo />
       </div>
-      <div
-        className={
-          "navbar-items-container" + (navbarMenuIsOpen ? "" : " is-hidden")
-        }
-      >
+      <div className={"navbar-items-container"}>
         <NavbarItem name="">Accueil</NavbarItem>
         <NavbarItem name="profile">Profil</NavbarItem>
         <NavbarItem name="recommandations">Recommandations</NavbarItem>
--- frontend/src/scenes/Homepage/Homepage.tsx
+++ frontend/src/scenes/Homepage/Homepage.tsx
@@ -2,11 +2,13 @@ import axios from "axios";
 import { v4 as uuidv4 } from "uuid";
 import "./Homepage.css";
 
+import Navbar from "@components/Navbar/Navbar";
 import GameCard, { GameCardProps } from "@components/GameCard/GameCard";
 
 import homepageVideoGamesBackground from "./assets/img/video_games_background.jpg";
 import { useEffect, useState } from "react";
 import { useDebounce } from "usehooks-ts";
+//import Footer from "@components/Footer/Footer";
 
 function Homepage() {
   const [searchText, setSearchText] = useState("");
@@ -16,38 +18,10 @@ function Homepage() {
 
   const [games, setGames] = useState([]);
 
-  const [scrollTop, setScrollTop] = useState(window.scrollY);
-
   const searchInputHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
     const newSearchText = e.target.value;
     setSearchText(newSearchText);
   };
-
-  // useEffect to update scrollTop
-  useEffect(() => {
-    const handleScroll = () => {
-      setScrollTop(window.scrollY);
-    };
-
-    window.addEventListener("scroll", handleScroll);
-
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
-
-  // useEffect to toggle navbar class .navbar-transparent
-  useEffect(() => {
-    if (scrollTop < 50) {
-      document
-        .getElementById("navbar-container")
-        ?.classList.add("navbar-transparent");
-    } else {
-      document
-        .getElementById("navbar-container")
-        ?.classList.remove("navbar-transparent");
-    }
-  }, [scrollTop]);
 
   useEffect(() => {
     let ignore = false;
@@ -100,43 +74,48 @@ function Homepage() {
   }, [debouncedSearchText]);
 
   return (
-    <div className="homepage-container">
-      <div
-        className="homepage-background-image"
-        style={{
-          backgroundImage: `linear-gradient(to bottom, rgba(0,0,0,0.5), rgba(0,0,0,0.2)), url(${homepageVideoGamesBackground})`,
-        }}
-      ></div>
-      <div className="homepage-games-outer-container">
-        <div className="homepage-searchbar">
-          <input
-            type="text"
-            value={searchText}
-            onChange={searchInputHandler}
-            placeholder="Elden Ring, The Last of Us..."
-          />
+    <>
+      <Navbar backgroundChangesOnScroll={false} />
+
+      <div className="homepage-container">
+        <div
+          className="homepage-background-image"
+          style={{
+            backgroundImage: `linear-gradient(to bottom, rgba(0,0,0,0.5), rgba(0,0,0,0.2)), url(${homepageVideoGamesBackground})`,
+          }}
+        ></div>
+        <div className="homepage-games-outer-container">
+          <div className="homepage-searchbar">
+            <input
+              type="text"
+              value={searchText}
+              onChange={searchInputHandler}
+              placeholder="Elden Ring, The Last of Us..."
+            />
+          </div>
+          <div className="homepage-games-inner-container">
+            {games.length > 0 ? (
+              games.map(({ gameName, gameImageLink }: GameCardProps) => {
+                return (
+                  <GameCard
+                    gameName={gameName}
+                    gameImageLink={gameImageLink}
+                    key={uuidv4()}
+                  />
+                );
+              })
+            ) : !dataArrived ? (
+              <div className="homepage-games-loading">Loading...</div>
+            ) : (
+              <div className="homepage-games-no-result">
+                No result for "{debouncedSearchText}"...
+              </div>
+            )}
+          </div>
         </div>
-        <div className="homepage-games-inner-container">
-          {games.length > 0 ? (
-            games.map(({ gameName, gameImageLink }: GameCardProps) => {
-              return (
-                <GameCard
-                  gameName={gameName}
-                  gameImageLink={gameImageLink}
-                  key={uuidv4()}
-                />
-              );
-            })
-          ) : !dataArrived ? (
-            <div className="homepage-games-loading">Loading...</div>
-          ) : (
-            <div className="homepage-games-no-result">
-              No result for "{debouncedSearchText}"...
-            </div>
-          )}
-        </div>
+        {/*<Footer />*/}
       </div>
-    </div>
+    </>
   );
 }
 

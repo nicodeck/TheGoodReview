e01c210c980576e834b179b34981f0a6a8e469db
Nicolas Descamps <nicolas.descamps@student-cs.fr>
2023-12-15 00:23:37 +0100

    feat(frontend): Add ProvideAuth context to App component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,11 @@
+import { ProvideAuth } from "hooks/useAuth";
 import { Outlet } from "react-router";
 
 function App() {
   return (
-    <>
+    <ProvideAuth>
       <Outlet />
-    </>
+    </ProvideAuth>
   );
 }
 

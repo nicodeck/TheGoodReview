a83ee4d1e4ef2e0b46484c3a191c78429036e893
Nicolas Descamps <nicolas.descamps@student-cs.fr>
2023-12-15 19:14:18 +0100

    feat(frontend): Remove authentication middleware from /homepage route

--- backend/index.js
+++ backend/index.js
@@ -19,11 +19,12 @@ app.use("/images", images);
 
 app.use("/auth", auth);
 
-app.get("/homepage", authentication, cors(), async (req, res) => {
+app.get("/homepage", cors(), async (req, res) => {
   const rawHomepageGamesData = await igdb_api_request(
     "/games",
     "fields name, cover.image_id; sort total_rating desc; where aggregated_rating_count >= 7 & first_release_date > 1104534000; limit 20;"
   );
+
   const cleanHomepageGamesData = rawHomepageGamesData.map((game) => {
     return {
       gameImageLink: `https://images.igdb.com/igdb/image/upload/t_cover_big/${game.cover.image_id}.jpg`,
@@ -31,6 +32,7 @@ app.get("/homepage", authentication, cors(), async (req, res) => {
       gameId: game.id,
     };
   });
+
   res.send({ games: cleanHomepageGamesData });
 });
 

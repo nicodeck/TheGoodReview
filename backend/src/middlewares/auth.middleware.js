5214cd6016a2112e93d6b9caa646cd809ee4415b
Nicolas Descamps <nicolas.descamps@student-cs.fr>
2023-12-15 10:11:00 +0100

    feat(backend): Refactor authentication middleware to handle missing or expired tokens

--- backend/src/middlewares/auth.middleware.js
+++ backend/src/middlewares/auth.middleware.js
@@ -2,18 +2,38 @@ const jwt = require("jsonwebtoken");
 
 function authentication(req, res, next) {
   console.log("Attempting authentication...");
+
   try {
+    // Check if the request has the Authorization header
+    if (!req.headers.authorization) {
+      req.auth = {
+        isAuth: false,
+      };
+      next();
+    }
+
+    // Extract the token from the Authorization header
     const token = req.headers.authorization.split(" ")[1];
     const decodedToken = jwt.verify(token, process.env.JWT_KEY);
     const username = decodedToken.username;
+    const iat = decodedToken.iat;
+
+    if (iat * 1000 < Date.now() - 1000 * 60 * 60) {
+      req.auth = {
+        isAuth: false,
+      };
+      next();
+    }
+
     req.auth = {
+      isAuth: true,
       username: username,
     };
-    console.log("token: ", token);
+
     next();
   } catch (error) {
     console.log(error);
-    res.status(401).send({ message: "Authentication failed." });
+    res.status(400).send({ message: "Authentication failed." });
   }
 }
 

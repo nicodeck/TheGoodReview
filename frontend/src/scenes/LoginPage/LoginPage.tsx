81d131973332827d0214501af1b98e76987427c2
Nicolas Descamps <nicolas.descamps@student-cs.fr>
2023-12-16 09:14:03 +0100

    feat(frontend): Add login basic functionality to LoginPage component

--- frontend/src/scenes/LoginPage/LoginPage.tsx
+++ frontend/src/scenes/LoginPage/LoginPage.tsx
@@ -1,14 +1,32 @@
-import { Form } from "react-router-dom";
+import { useState } from "react";
+import { Form, useNavigate } from "react-router-dom";
 import Logo from "@components/Navbar/components/Logo/Logo";
+import { useAuth } from "@hooks/useAuth";
+
 import "./LoginPage.css";
 
 function LoginPage() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const { login } = useAuth();
+
+  const navigate = useNavigate();
+
+  async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    const loginResponse = await login(username, password);
+    if (loginResponse.isAuth) {
+      navigate("/");
+    }
+  }
+
   return (
     <div className="login-page-container">
       <div className="login-page-logo">
         <Logo />
       </div>
-      <Form className="login-page-form" method="post" action="/login">
+      <Form className="login-page-form" onSubmit={handleSubmit}>
         <label className="login-page-form-label" htmlFor="username">
           Username
         </label>
@@ -17,6 +35,8 @@ function LoginPage() {
           type="text"
           id="username"
           name="username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
         />
         <label className="login-page-form-label" htmlFor="password">
           Password
@@ -26,6 +46,8 @@ function LoginPage() {
           type="password"
           id="password"
           name="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
         />
         <button className="login-page-form-submit-button" type="submit">
           Login

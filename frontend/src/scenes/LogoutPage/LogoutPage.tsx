56eec7dfde4f7cd8ed5f5b10067f7d8a7b392b1a
Nicolas Descamps <nicolas.descamps@student-cs.fr>
2023-12-17 16:02:05 +0100

    feat(frontend): Add LogoutPage component and styles

---
+++ frontend/src/scenes/LogoutPage/LogoutPage.tsx
@@ -0,0 +1,41 @@
+import Logo from "@components/Navbar/components/Logo/Logo";
+import { useEffect, useState } from "react";
+import { useAuth } from "@hooks/useAuth";
+import "./LogoutPage.css";
+import { Link, useNavigate } from "react-router-dom";
+
+function LogoutPage() {
+  const [isLoggedOut, setIsLoggedOut] = useState(false);
+
+  const { logout } = useAuth();
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (logout()) {
+      setIsLoggedOut(true);
+      setTimeout(() => {
+        navigate("/");
+      }, 2000);
+    } else {
+      console.log("Error logging out");
+    }
+  }, []);
+
+  return (
+    <div className="logout-page-container">
+      <div className="logout-page-logo">
+        <Link to="/">
+          <Logo />
+        </Link>
+      </div>
+      <div className="logout-page-text">
+        {isLoggedOut
+          ? "You have successfully been logged out. Redirecting to homepage..."
+          : "Disconnecting..."}
+      </div>
+    </div>
+  );
+}
+
+export default LogoutPage;

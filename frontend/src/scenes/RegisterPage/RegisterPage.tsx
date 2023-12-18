5a9f6e3cb87804f86c02ba580591ec8cd48e9e19
Nicolas Descamps <nicolas.descamps@student-cs.fr>
2023-12-18 11:19:18 +0100

    feat(frontend): Update RegisterPage styles and invalid fields errors

--- frontend/src/scenes/RegisterPage/RegisterPage.tsx
+++ frontend/src/scenes/RegisterPage/RegisterPage.tsx
@@ -1,7 +1,8 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Link, Form } from "react-router-dom";
 import Logo from "@components/Navbar/components/Logo/Logo";
 import "./RegisterPage.css";
+import { IoCloseCircleOutline } from "react-icons/io5";
 
 function RegisterPage() {
   const [formUsername, setFormUsername] = useState("");
@@ -12,17 +13,56 @@ function RegisterPage() {
   const [missingEmail, setMissingEmail] = useState(false);
   const [missingPassword, setMissingPassword] = useState(false);
 
-  const handleSubmit = () => {
+  const [validEmail, setValidEmail] = useState(true);
+
+  const [registering, setRegistering] = useState(false);
+
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
     if (!formUsername) {
       setMissingUsername(true);
+    } else {
+      setMissingUsername(false);
     }
     if (!formEmail) {
       setMissingEmail(true);
+    } else {
+      setMissingEmail(false);
     }
     if (!formPassword) {
       setMissingPassword(true);
+    } else {
+      setMissingPassword(false);
     }
+
+    if (!formEmail || emailRegex.test(formEmail)) {
+      setValidEmail(true);
+    } else {
+      setValidEmail(false);
+    }
+
+    setRegistering(true);
   };
+
+  useEffect(() => {
+    if (
+      registering &&
+      formUsername &&
+      formEmail &&
+      formPassword &&
+      validEmail
+    ) {
+      console.log("registering");
+      setTimeout(() => {
+        console.log("done registering");
+        setRegistering(false);
+      }, 5000);
+    } else {
+      setRegistering(false);
+    }
+  }, [registering, formUsername, formEmail, formPassword, validEmail]);
 
   return (
     <div className="register-page-container">
@@ -31,23 +71,35 @@ function RegisterPage() {
           <Logo />
         </Link>
       </div>
-      {missingEmail || missingUsername || missingPassword ? (
+      {missingEmail || missingUsername || missingPassword || !validEmail ? (
         <div className="register-error-container">
-          Unvalid username or password.
+          <div className="register-error-title">
+            Invalid fields. Please fix the following:
+          </div>
+
+          <ul className="register-error-invalid-fields">
+            {missingUsername ? <li>Enter username</li> : null}
+            {missingEmail ? <li>Enter email</li> : null}
+            {missingPassword ? <li>Enter password</li> : null}
+            {!validEmail ? <li>Enter valid email</li> : null}
+          </ul>
         </div>
       ) : null}
       <Form className="register-page-form" onSubmit={handleSubmit}>
-        <label
-          className={
-            "register-page-form-label" +
-            (missingUsername ? " register-page-missing-field" : "")
-          }
-          htmlFor="username"
-        >
+        <label className={"register-page-form-label"} htmlFor="username">
           Username
         </label>
+        {missingUsername ? (
+          <div className="register-page-invalid-field-alert">
+            <IoCloseCircleOutline />
+            Missing username
+          </div>
+        ) : null}
         <input
-          className="register-page-form-input"
+          className={
+            "register-page-form-input" +
+            (missingUsername ? " register-page-invalid-field" : "")
+          }
           type="text"
           id="username"
           name="username"
@@ -58,10 +110,22 @@ function RegisterPage() {
         <label className="register-page-form-label" htmlFor="email">
           Email
         </label>
+        {missingEmail ? (
+          <div className="register-page-invalid-field-alert">
+            <IoCloseCircleOutline />
+            Missing email
+          </div>
+        ) : null}
+        {validEmail ? null : (
+          <div className="register-page-invalid-field-alert">
+            <IoCloseCircleOutline />
+            Invalid email
+          </div>
+        )}
         <input
           className={
             "register-page-form-input" +
-            (missingEmail ? " register-page-missing-field" : "")
+            (missingEmail || !validEmail ? " register-page-invalid-field" : "")
           }
           type="text"
           id="email"
@@ -72,10 +136,16 @@ function RegisterPage() {
         <label className="register-page-form-label" htmlFor="password">
           Password
         </label>
+        {missingPassword ? (
+          <div className="register-page-invalid-field-alert">
+            <IoCloseCircleOutline />
+            Missing password
+          </div>
+        ) : null}
         <input
           className={
             "register-page-form-input" +
-            (missingEmail ? " register-page-missing-field" : "")
+            (missingPassword ? " register-page-invalid-field" : "")
           }
           type="password"
           id="password"
@@ -83,8 +153,18 @@ function RegisterPage() {
           value={formPassword}
           onChange={(e) => setFormPassword(e.target.value)}
         />
-        <button className="register-page-form-submit-button" type="submit">
-          Register
+        <button
+          className={
+            "register-page-form-submit-button" +
+            (registering ? " register-page-form-loading" : "")
+          }
+          type="submit"
+        >
+          {registering ? (
+            <div className="register-page-form-submit-button-loading"></div>
+          ) : (
+            "Register"
+          )}
         </button>
       </Form>
     </div>

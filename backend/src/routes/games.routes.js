4c09d389d465d8445ed3948beaca3ad7d96877b6
Nicolas Descamps <nicolas.descamps@student-cs.fr>
2023-12-19 13:22:54 +0100

    feat(backend): Add authentication middleware and like functionality

--- backend/src/routes/games.routes.js
+++ backend/src/routes/games.routes.js
@@ -1,9 +1,13 @@
 const express = require("express");
-const cors = require("cors");
 
 const router = express.Router();
 
 const { igdb_api_request } = require("../utils/igdb_request.utils");
+const authentication = require("../middlewares/auth.middleware");
+
+const { PrismaClient } = require("@prisma/client");
+
+const prisma = new PrismaClient();
 
 router.get("/", async (req, res) => {
   const fields = req.query.fields ? req.query.fields : "*";
@@ -17,10 +21,10 @@ router.get("/", async (req, res) => {
   res.send(gameData);
 });
 
-router.get("/gamecard", cors(), async (req, res) => {
+router.get("/gamecard", authentication, async (req, res) => {
   const fields =
     "name, cover.image_id, first_release_date, summary, aggregated_rating";
-  const id = req.query.id ? req.query.id : 1942;
+  const id = req.query.id ? Number(req.query.id) : 1942;
 
   console.log("Requesting details of game with id: ", id);
 
@@ -30,6 +34,19 @@ router.get("/gamecard", cors(), async (req, res) => {
       `fields ${fields}; where id=${id};`
     );
 
+    console.log("Request auth: ", req.auth);
+
+    const like = req.auth.isAuth
+      ? await prisma.like.findUnique({
+          where: {
+            likeId: {
+              gameId: id,
+              authorId: req.auth.userId,
+            },
+          },
+        })
+      : null;
+
     const cleanGameCardData = {
       gameImageLink: `https://images.igdb.com/igdb/image/upload/t_cover_big/${rawGameCardData[0].cover.image_id}.jpg`,
       gameName: rawGameCardData[0].name,
@@ -38,6 +55,7 @@ router.get("/gamecard", cors(), async (req, res) => {
       ).getFullYear(),
       gameDescription: rawGameCardData[0].summary,
       gameGrade: Math.round(rawGameCardData[0].aggregated_rating) / 10,
+      gameLiked: like ? like.like : false,
     };
 
     res.send(cleanGameCardData);
@@ -45,6 +63,58 @@ router.get("/gamecard", cors(), async (req, res) => {
     console.log(error);
     res.status(500).send();
   }
+});
+
+router.post("/like", express.json(), authentication, async (req, res) => {
+  const isAuth = req.auth.isAuth;
+  if (!isAuth) {
+    res.status(401).send({ message: "Unauthorized." });
+    return;
+  }
+
+  const userId = req.auth.userId;
+  const gameId = req.body.gameId;
+  const liked = req.body.liked;
+
+  const game = await prisma.game.upsert({
+    where: {
+      id: gameId,
+    },
+    update: {},
+    create: {
+      id: gameId,
+    },
+  });
+
+  const like = await prisma.like.upsert({
+    where: {
+      likeId: {
+        gameId: gameId,
+        authorId: userId,
+      },
+    },
+    update: {
+      like: liked,
+    },
+    create: {
+      game: {
+        connect: {
+          id: gameId,
+        },
+      },
+      author: {
+        connect: {
+          id: userId,
+        },
+      },
+
+      like: liked,
+    },
+  });
+
+  console.log("Like: ", like);
+
+  res.status(200).send();
 });
 
 module.exports = router;
